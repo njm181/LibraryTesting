f3780eef70d6f9878323b29a4365b7100aa46fd0
Nicolas Molina <nicolasjmolina1@gmail.com>
2024-07-30 17:06:33 -0300

    Work Done

---
+++ MyLibrary/MyLibrary/Core.swift
@@ -0,0 +1,12 @@
+//
+//  Core.swift
+//  MyLibrary
+//
+//  Created by Nico Molina on 30/07/2024.
+//
+
+import Foundation
+
+public func printString(value: String) {
+    print(value)
+}
---
+++ MyLibrary/MyLibrary/MyLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MyLibrary.h
+//  MyLibrary
+//
+//  Created by Nico Molina on 30/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLibrary.
+FOUNDATION_EXPORT double MyLibraryVersionNumber;
+
+//! Project version string for MyLibrary.
+FOUNDATION_EXPORT const unsigned char MyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLibrary/PublicHeader.h>
+
+
